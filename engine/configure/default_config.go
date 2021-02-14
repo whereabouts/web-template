028008f53976c56cadb6d51550e3f9d8fd6584d0
hezebin <86744316@qq.com>
2021-02-15 00:34:35 +0800

    fix readFile path

--- engine/configure/default_config.go
+++ engine/configure/default_config.go
@@ -2,27 +2,30 @@ package configure
 
 import (
 	"encoding/json"
+	"github.com/gin-gonic/gin"
 	"github.com/whereabouts/chassis/logger"
 	"io/ioutil"
 	"reflect"
 )
 
 const (
-	fieldNamePort             = "Port"
-	fieldNameEnv              = "Env"
-	defaultConfigPath         = "config/application.json"
-	defaultInternalConfigPath = "engine/configure/application.json"
+	fieldNamePort     = "Port"
+	fieldNameEnv      = "Env"
+	defaultConfigPath = "./config/application.json"
+	//defaultInternalConfigPath = "./engine/configure/application.json"
 )
 
 func init() {
-	data, err := ioutil.ReadFile(defaultInternalConfigPath)
-	if err != nil {
-		logger.Fatalf("read internal default config file err: %+v", err)
-	}
-	err = json.Unmarshal(data, &dConfig)
-	if err != nil {
-		logger.Fatalf("unmarshal internal default config err: %+v", err)
-	}
+	//data, err := ioutil.ReadFile(defaultInternalConfigPath)
+	//if err != nil {
+	//	logger.Fatalf("read internal default config file err: %+v", err)
+	//}
+	//err = json.Unmarshal(data, &dConfig)
+	//if err != nil {
+	//	logger.Fatalf("unmarshal internal default config err: %+v", err)
+	//}
+	dConfig.Env = gin.DebugMode
+	dConfig.Port = 8080
 }
 
 type IConfig interface {
@@ -43,6 +46,7 @@ func Load(conf IConfig) (*DefaultConfig, error) {
 	// if fail to read the default configure file, that will appoint some default values
 	if err != nil {
 		if cmdParam.String() == defaultConfigPath {
+			logger.Warning("read file: ./config/application.json err, but the default configuration is selected!")
 			return DefaultLoad(), nil
 		}
 		return nil, err
