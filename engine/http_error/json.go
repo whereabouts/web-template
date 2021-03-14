74a3f644d3cc6fef72ca40e5d7cd3d6e2d9eb659
hezebin <86744316@qq.com>
2021-03-14 23:38:34 +0800

    Add default response body json

--- engine/http_error/json.go
+++ engine/http_error/json.go
@@ -1,12 +1,10 @@
 package http_error
 
-type DefaultBody struct {
-	Code    interface{} `json:"code"`
-	Message string      `json:"message"`
-}
-
 type JSON map[string]interface{}
 
-func DefaultJSON(code interface{}, message string) DefaultBody {
-	return DefaultBody{Code: code, Message: message}
+func DefaultJSON(code interface{}, message string) interface{} {
+	return struct {
+		Code    interface{} `json:"err_code"`
+		Message string      `json:"err_message"`
+	}{code, message}
 }
